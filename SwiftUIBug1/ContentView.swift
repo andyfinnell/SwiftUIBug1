ceda3aa8a74f4f2b1d8f9843b74a1d41f86c712c
Andrew Finnell <andy@losingfight.com>
2020-08-15 20:05:47 -0400

    Initial Commit

---
+++ SwiftUIBug1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBug1
+//
+//  Created by Andrew Finnell on 8/15/20.
+//  Copyright © 2020 Safe from the Losing Fight. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
